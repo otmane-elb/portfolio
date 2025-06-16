bea22fbb6e111fab19106cc37ba7571c2bf62879
otmane elbaghazaoui <otmane.elbaghazaoui@intelcia.com>
2025-06-16 18:02:05 +0100

    add anylitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Analytics } from "@vercel/analytics/next"
 
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
